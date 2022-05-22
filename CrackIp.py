9921d2315c1ab782a1f982c3fc0677af0d7e1fe0
bayonnn <105988400+bayonnn@users.noreply.github.com>
2022-05-22 13:08:39 +0700

    Add files via upload

---
+++ CrackIp.py
@@ -0,0 +1,25 @@
+import random
+import socket
+import threading
+import time
+import os,sys
+
+os.system('clear')
+print("\033[1;31;40m")
+print("""
+
+╭━━━━╮╱╱╱╱╱╱╱╭━╮╭━╮
+╰━━╮━┃╱╱╱╱╱╱╱╰╮╰╯╭╯
+╱╱╭╯╭╋━━┳━┳━━╮╰╮╭╯
+╱╭╯╭╯┃┃━┫╭┫╭╮┃╭╯╰╮
+╭╯━╰━┫┃━┫┃┃╰╯┣╯╭╮╰╮
+╰━━━━┻━━┻╯╰━━┻━╯╰━╯""")
+print("UNTUK MELIHAT IP WEBSITE,CARANYA SANGAT MUDAH")
+print("KALIAN HANYA HARUS KETIK ping NAMA WEBSITE")
+print("CONTOH : ping detik.com")
+print("JIKA SUDAH KALIAN KETIK SEPERTI ITU,AKAN MUNCUL IP WEBSITE TERSEBUT")
+print("GUNA NYA CRACK IP ADALAH UNTUK MELAKUKAN SERANGAN DDoS DAN MENDAPATKAN INFORMASI WEBSITE TERSEBUT")
+print("JANGAN LUPA SUBSCRIBE YOUTUBE Gada Lu Bau")
+print("MASUK JUGA GRUP WA SAYA AGAR MENDAPATKAN INFORMASI LEBIH LANJUT")
+print("LINK GRUP : https://chat.whatsapp.com/JXIxGjGN9oF7rEy3ACQH33")
+print("MAKASIH TELAH MENGGUNAKAN TOOLS INI")
